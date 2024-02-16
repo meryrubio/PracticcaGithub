f8a1d54f546fefe01934d7c8185f53de60f73176
Alvergel25 <153915092+Alvergel25@users.noreply.github.com>
2024-02-16 13:00:14 +0100

    ArribaW

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+
+    public KeyCode upkey = KeyCode.W;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upkey))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+    }
+}
